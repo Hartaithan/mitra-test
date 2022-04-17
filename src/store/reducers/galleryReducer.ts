9a908008b10b91057701268b7ac072a087a993c8
hartaithan <hartaithan@gmail.com>
2022-04-17 15:27:33 +0900

    split photos array by albumIds

--- src/store/reducers/galleryReducer.ts
+++ src/store/reducers/galleryReducer.ts
@@ -1,6 +1,7 @@
 import {
   GalleryAction,
   GalleryActionTypes,
+  IGalleryItem,
   IGalleryState,
 } from "../../models/galleryModel";
 
@@ -8,6 +9,24 @@ const initialState: IGalleryState = {
   list: [],
   isLoading: false,
   error: null,
+};
+
+const splitByAlbumIds = (array: IGalleryItem[]): IGalleryItem[] => {
+  const filteredItems: IGalleryItem[] = [...array].map((item, index) => {
+    switch (true) {
+      case 0 <= index && index < 6:
+        return { ...item, albumId: 1 };
+      case 6 <= index && index < 12:
+        return { ...item, albumId: 2 };
+      case 12 <= index && index < 18:
+        return { ...item, albumId: 3 };
+      case 18 <= index && index < 24:
+        return { ...item, albumId: 4 };
+      default:
+        return item;
+    }
+  });
+  return filteredItems;
 };
 
 const galleryReducer = (
@@ -18,11 +37,15 @@ const galleryReducer = (
     case GalleryActionTypes.GET_GALLERY:
       return { ...state, isLoading: true, list: [], error: null };
     case GalleryActionTypes.GET_GALLERY_SUCCESS:
-      return { ...state, isLoading: false, list: action.payload, error: null };
+      return {
+        ...state,
+        isLoading: false,
+        list: splitByAlbumIds(action.payload),
+        error: null,
+      };
     case GalleryActionTypes.GET_GALLERY_ERROR:
       return { ...state, isLoading: false, list: [], error: action.payload };
     default:
-      console.error("galleryReducer switch case error");
       return state;
   }
 };
