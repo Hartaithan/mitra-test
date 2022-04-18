75f98eb471e710994b90eb5fb23fe86fa8a53b90
Hartaithan <hartaithan@gmail.com>
2022-04-18 09:33:04 +0000

    add overlay on grid items

--- src/components/GalleryGrid/GalleryGrid.tsx
+++ src/components/GalleryGrid/GalleryGrid.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import { IGalleryGridProps } from "../../models/galleryGridModel";
 import "./galleryGrid.scss";
 
@@ -6,7 +8,20 @@ const GalleryGrid: React.FC<IGalleryGridProps> = ({ list }) => {
   return (
     <div className="galleryGrid">
       {list.map((item) => (
-        <img key={item.id} src={item.url} alt={item.id.toString()} />
+        <div className="galleryGrid__item" key={item.id}>
+          <div className="galleryGrid__item__overlay">
+            <p className="galleryGrid__item__album">{`Album ${item.albumId}`}</p>
+            <p className="galleryGrid__item__title">{item.title}</p>
+            <NavLink to={`/details/${item.id}`}>
+              <Button className="galleryGrid__item__button">Read more</Button>
+            </NavLink>
+          </div>
+          <img
+            className="galleryGrid__item__pic"
+            src={item.url}
+            alt={item.id.toString()}
+          />
+        </div>
       ))}
     </div>
   );
