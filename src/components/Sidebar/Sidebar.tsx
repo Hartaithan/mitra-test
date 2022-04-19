bef6998fe8b7efc6402c4e77a484471eac4429d3
Hartaithan <hartaithan@gmail.com>
2022-04-19 03:22:21 +0000

    use bootstrap components in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Card, Image, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./sidebar.scss";
 
@@ -11,20 +11,21 @@ const Sidebar: React.FC = () => {
       placement="start"
       aria-labelledby="headerNavMenu"
     >
-      <Offcanvas.Header closeButton>
-        <div className="sidebar__user">
-          <Image
-            className="sidebar__user__avatar"
-            src="/img/user.png"
-            rounded
-          />
-          <div className="sidebar__user__details">
-            <p>John Doe</p>
-            <p>johndoe@gmail.com</p>
-          </div>
-        </div>
-      </Offcanvas.Header>
+      <Offcanvas.Header closeButton>Navigation Menu</Offcanvas.Header>
       <Offcanvas.Body>
+        <Card className="sidebar__user w-100 mb-2">
+          <Card.Body>
+            <Image
+              className="sidebar__user__avatar mb-3"
+              src="/img/user.png"
+              rounded
+            />
+            <Card.Title>Vladimir Stepanov</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              stepanov.vladimir.in.re@gmail.com
+            </Card.Subtitle>
+          </Card.Body>
+        </Card>
         <Nav className="justify-content-end flex-grow-1 pe-3">
           <NavLink className="nav-link" to="/">
             Gallery
